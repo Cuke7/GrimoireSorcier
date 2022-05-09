6b875dd4aaae17fde089a54057a3de05ce1066da
Cuke7 <cuke.dev@mail.com>
2022-05-09 16:19:25 +0200

    Demo thomas

--- src/components/GrimoireSorcier.vue
+++ src/components/GrimoireSorcier.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="ma-12">
         <div class="text-h2 my-6 text-center">Grimoire du sorcier</div>
-        <v-row justify="center">
+        <v-row>
             <v-col cols="6">
                 <v-row justify="center">
                     <v-col cols="8">
@@ -12,29 +12,43 @@
                     <v-col cols="auto">
                         <v-switch v-model="sortLevel" label="Trier par niveau" color="blue" hide-details> </v-switch>
                     </v-col>
-                    <v-col cols="3">
+                    <v-col cols="auto">
                         <v-text-field v-model="spellLevel" label="Niveau du sort" type="number" min="1" max="10" hide-details :disabled="!sortLevel"></v-text-field>
                     </v-col>
                 </v-row>
 
                 <v-divider class="my-6"></v-divider>
                 <div v-for="(searchResult, index) in searchResults" :key="index">
-                    {{ searchResult.item.translations.fr.name }}
+                    <div @click="selectedSpell = searchResult.item">
+                        <v-row align="center">
+                            <v-col cols="auto">
+                                <v-avatar color="light-blue"> {{ searchResult.item.level }} </v-avatar>
+                            </v-col>
+                            <v-col cols="auto">
+                                {{ searchResult.item.translations.fr.name }}
+                            </v-col>
+                        </v-row>
+                    </div>
+                    <v-divider class="my-4"></v-divider>
                 </div>
+            </v-col>
+            <v-col cols="6">
+                <SpellPreview :selectedSpell="selectedSpell" />
             </v-col>
         </v-row>
     </div>
 </template>
 
 <script setup>
+import SpellPreview from "./SpellPreview.vue";
 import { ref, computed } from "vue";
 import spells_srd from "../assets/spells-srd.json";
-import { onMounted } from "vue";
 
 let searchQuery = ref("");
 let spellLevel = ref(1);
-let sortLevel = ref(true);
+let sortLevel = ref(false);
 
+// Liste de sorts de la tradition arcaniste, rituels et sorts focalisés exclus
 const spellList = computed(() => {
     let spells = spells_srd;
     spells = spells.filter((spell) => spell.type == "spell");
@@ -48,18 +62,48 @@ const spellList = computed(() => {
 });
 
 const { Searcher } = require("fast-fuzzy");
+// Objet nécessaire pour faire la recherche
 const searcher = computed(() => {
     return new Searcher(spellList.value, {
-        keySelector: (obj) => obj.translations.fr.name,
-    });
-});
-const searchResults = computed(() => {
-    return searcher.value.search(searchQuery.value, {
-        returnMatchData: true,
+        keySelector: (obj) => obj.translations.fr.name, // On fait la recherche sur le nom du sort
     });
 });
 
-onMounted(() => {
-    //
+// Tableau avec les résultats de la recherche, on en affiche que 5
+const searchResults = computed(() => {
+    return searcher.value
+        .search(searchQuery.value, {
+            returnMatchData: true,
+        })
+        .slice(0, 5);
 });
+
+// Contient le dernier sort cliqué pour l'afficher dans le cadre à droite
+let selectedSpell = ref(null);
 </script>
+
+<style scoped>
+#card {
+    overflow: auto;
+}
+
+/* Permet de customiser un peu la scrollbar/*
+/* width */
+::-webkit-scrollbar {
+    width: 10px;
+}
+/* Track */
+::-webkit-scrollbar-track {
+    box-shadow: inset 0 0 5px grey;
+    border-radius: 20px;
+}
+/* Handle */
+::-webkit-scrollbar-thumb {
+    background: grey;
+    border-radius: 10px;
+}
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+    background: #361268;
+}
+</style>
---
+++ src/components/SpellPreview.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-card class="pa-2" height="600" id="card">
+        <div v-if="props.selectedSpell">
+            <v-card-title>
+                {{ spell.name }}
+            </v-card-title>
+            <div class="font-weight-bold ma-4">Niveau : {{ level }}</div>
+            <div v-for="(key, index) in Object.keys(spell)" :key="index" class="ml-4">
+                <v-chip v-if="key != 'status' && key != 'name' && key != 'description'" class="my-2"> {{ key }} : {{ spell[key] }} </v-chip>
+            </div>
+            <v-card-text v-html="spell.description"></v-card-text>
+        </div>
+    </v-card>
+</template>
+
+<script setup>
+import { defineProps, computed } from "vue";
+const props = defineProps(["selectedSpell"]);
+
+// Décompose la props, par soucis de simplicité
+let spell = computed(() => {
+    return props.selectedSpell.translations.fr;
+});
+let level = computed(() => {
+    return props.selectedSpell.level;
+});
+</script>
